fef483d8ff701b6ce8f7a1b279d38a1dd762d13a
Alexey Gorishny <0x414c@gmail.com>
2017-12-01 23:08:52 +0300

    docs(logger): move TODO to corresponding file

--- sources/utils/logging/logger.hxx
+++ sources/utils/logging/logger.hxx
@@ -24,12 +24,6 @@ namespace Utils
     public:
       /**
        * @brief
-       * TODO: [0;0] Use
-       *   `Q_FUNC_INFO' (from `QtGlobal') or
-       *   `BOOST_CURRENT_FUNCTION' (from `boost/current_function.hpp') or
-       *   `__PRETTY_FUNCTION__' or
-       *   `__FUNCTION__' or
-       *   `__func__'?
        * @param function
        * @param file
        * @param line
