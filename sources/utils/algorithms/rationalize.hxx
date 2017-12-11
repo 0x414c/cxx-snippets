dc7103528915704d448fbf425df0d632cef6ffd3
Alexey Gorishny <0x414c@gmail.com>
2017-12-11 21:08:11 +0300

    docs(raitionalize): add comments

--- sources/utils/algorithms/rationalize.hxx
+++ sources/utils/algorithms/rationalize.hxx
@@ -17,6 +17,20 @@
 
 namespace Utils
 {
+  /**
+   * @brief Finds the "best" rational approximation (p / q) of the real number `x' that satisfies given restrictions
+   *   (error, maximal denominator value and maximal iterations count) by using Continued Fractions method.
+   * For the reference see `http://mathworld.wolfram.com/ContinuedFraction.html'.
+   * The absolute error value will be |p / q - x|.
+   * @tparam TInt
+   * @tparam TFloat
+   * @param x
+   * @param max_denominator
+   * @param max_iterations
+   * @param rel_tol
+   * @param abs_tol
+   * @return
+   */
   template <typename TInt, typename TFloat>
   constexpr std::tuple <TInt, TInt, TFloat>
   rationalize (
@@ -42,7 +56,7 @@ namespace Utils
 
     if (!std::isfinite (x))
     {
-      return { 0, 0, std::numeric_limits <TFloat>::quiet_NaN () };
+      return { TInt (0), TInt (0), std::numeric_limits <TFloat>::quiet_NaN () };
     }
 
 #pragma GCC diagnostic push
@@ -50,44 +64,60 @@ namespace Utils
     if (x == TFloat (0))
 #pragma GCC diagnostic pop
     {
-      return { 0, 1, 0 };
+      return { TInt (0), TInt (1), TFloat (0) };
     }
 
-    TFloat r_1 (x);
+    TFloat r_1 (x); // See eq. (8): r[0] := x
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wfloat-conversion"
-    TInt a_1 (std::trunc (r_1));
+    TInt a_1 (std::trunc (r_1)); // See eq. (5): a[0] := ⌊x⌋
 #pragma GCC diagnostic pop
 
+    /*
+     * If we got "almost integer" `x' or one of the trivial cases, we should return pair 〈a0; 1, e〉,
+     *   or the loop will stuck at division by 0 later.
+     */
     if (isClose (TFloat (a_1), x, rel_tol, abs_tol) || (max_denominator == 1) || (max_iterations == 1))
     {
-      return { a_1, 1, TFloat (a_1) - x };
+      return { a_1, TInt (1), TFloat (a_1) - x };
     }
 
     std::size_t iteration (1);
 
-    TInt p_0 (1);
-    TInt q_0 (0);
-    TInt p_1 (a_1);
-    TInt q_1 (1);
-    TInt p_2 (0);
-    TInt q_2 (1);
+    TInt p_0 (1); // See eq. (25): p[-1] := 1
+    TInt q_0 (0); // See eq. (25): q[-1] := 0
+    TInt p_1 (a_1); // See eq. (26): p[0] := a[0]
+    TInt q_1 (1); // See eq. (26): q[0] := 1
+    TInt p_2 (0); // p[1] := 0
+    TInt q_2 (1); // q[1] := 1
+    /*
+     * NOTE: Indices in variables' names is shifted by 1.
+     * We use only p[-1], q[-1] and p[0], q[0] as starting coefficients.
+     * Rather than keeping entire sequence of p's and q's we will keep and reuse coefficients computed on current
+     *   iteration (p[1] and q[1]) to compute new coefficients on the next iteration.
+     */
 
     while (true)
     {
       ++iteration;
 
-      const TFloat r_2 (TFloat (1) / (r_1 - TFloat (a_1)));
+      const TFloat r_2 (TFloat (1) / (r_1 - TFloat (a_1))); // See eq. (9): r[n] := 1 / (r[n-1] - a[n-1])
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wfloat-conversion"
-      const TInt a_2 (std::trunc (r_2));
+      const TInt a_2 (std::trunc (r_2)); // See eq. (10): a[n] := ⌊r[n]⌋
 #pragma GCC diagnostic pop
-      p_2 = a_2 * p_1 + p_0;
-      q_2 = a_2 * q_1 + q_0;
+      p_2 = a_2 * p_1 + p_0; // See eq. (27): p[n] == a[n] * p[n-1] + p[n-2]
+      q_2 = a_2 * q_1 + q_0; // See eq. (28): q[n] == a[n] * q[n-1] + q[n-2]
+      // Look at the n-th convergent (see eq. (11)): c[n] := p[n] / q[n]:
       const TFloat c_2 (TFloat (p_2) / TFloat (q_2));
 
       if (!isClose (c_2, x, rel_tol, abs_tol) && (abs (q_2) <= max_denominator) && (iteration < max_iterations))
       {
+        /* Now "shift" all the coefficients to the left:
+         *   - p[-1] := p[0], q[-1] := q[0];
+         *   - p[ 0] := p[1], q[ 0] := q[1];
+         *   - r[ 0] := r[1], a[ 0] := a[1].
+         */
         p_0 = p_1;
         q_0 = q_1;
         p_1 = p_2;
