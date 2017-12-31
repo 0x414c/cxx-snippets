81c531e1afecbb324321ac09fdf400b67b2debc4
Alexey Gorishny <0x414c@gmail.com>
2017-12-31 21:19:01 +0300

    feat(type-traits): add `RemoveCVRef' trait

---
+++ sources/utils/type-traits/remove-c-v-ref.hxx
@@ -0,0 +1,27 @@
+#ifndef UTILS_TYPETRAITS_REMOVECVREF_HXX
+#define UTILS_TYPETRAITS_REMOVECVREF_HXX
+
+
+#include <type_traits> // std::{remove_cv, remove_reference_t}
+
+#include "common.hxx" // TypeOf
+
+
+namespace Utils
+{
+  /**
+   * @brief
+   */
+  template <typename TType>
+  using RemoveCVRef = std::remove_cv <std::remove_reference_t <TType>>;
+
+
+  /**
+   * @brief
+   */
+  template <typename TType>
+  using RemoveCVRefT = TypeOf <RemoveCVRef <TType>>;
+}
+
+
+#endif // UTILS_TYPETRAITS_REMOVECVREF_HXX
