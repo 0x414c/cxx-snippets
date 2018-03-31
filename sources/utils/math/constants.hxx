47ac1bf39edb29ed4b1624149137ff61d23ea81d
Alexey Gorishny <0x414c@gmail.com>
2018-03-31 22:11:46 +0300

    docs(constants): fix type parameters

--- sources/utils/math/constants.hxx
+++ sources/utils/math/constants.hxx
@@ -15,84 +15,84 @@ namespace Utils::Math::Constants
 
   /**
    * @brief Binary logarithm of `e'.
-   * @tparam T
+   * @tparam TArithmetic
    */
   template <typename TArithmetic>
   inline constexpr TArithmetic Lb_e { 1.442695040888963407359924681001892137l };
 
   /**
    * @brief Common (base 10) logarithm of `e'.
-   * @tparam T
+   * @tparam TArithmetic
    */
   template <typename TArithmetic>
   inline constexpr TArithmetic Lg_e { 0.434294481903251827651128918916605082l };
 
   /**
    * @brief Natural logarithm of 2.
-   * @tparam T
+   * @tparam TArithmetic
    */
   template <typename TArithmetic>
   inline constexpr TArithmetic Ln_2 { 0.693147180559945309417232121458176568l };
 
   /**
    * @brief Natural logarithm of 10.
-   * @tparam T
+   * @tparam TArithmetic
    */
   template <typename TArithmetic>
   inline constexpr TArithmetic Ln_10 { 2.302585092994045684017991454684364208l };
 
   /**
    * @brief Archimedes' constant (π).
-   * @tparam T
+   * @tparam TArithmetic
    */
   template <typename TArithmetic>
   inline constexpr TArithmetic Pi { 3.141592653589793238462643383279502884l };
 
   /**
    * @brief π / 2.
-   * @tparam T
+   * @tparam TArithmetic
    */
   template <typename TArithmetic>
   inline constexpr TArithmetic Pi_div_2 { 1.570796326794896619231321691639751442l };
 
   /**
    * @brief π / 4.
-   * @tparam T
+   * @tparam TArithmetic
    */
   template <typename TArithmetic>
   inline constexpr TArithmetic Pi_div_4 { 0.785398163397448309615660845819875721l };
 
   /**
    * @brief 1 / π.
-   * @tparam T
+   * @tparam TArithmetic
    */
   template <typename TArithmetic>
   inline constexpr TArithmetic One_div_pi { 0.318309886183790671537767526745028724l };
 
   /**
    * @brief 2 / π.
-   * @tparam T
+   * @tparam TArithmetic
    */
   template <typename TArithmetic>
   inline constexpr TArithmetic Two_div_pi { 0.636619772367581343075535053490057448l };
 
   /**
    * @brief 2 / Sqrt[π].
-   * @tparam T
+   * @tparam TArithmetic
    */
   template <typename TArithmetic>
   inline constexpr TArithmetic Two_div_sqrt_pi { 1.128379167095512573896158903121545172l };
 
   /**
    * @brief Sqrt[2].
-   * @tparam T
+   * @tparam TArithmetic
    */
   template <typename TArithmetic>
   inline constexpr TArithmetic Sqrt_2 { 1.414213562373095048801688724209698079l };
 
   /**
    * @brief 1 / Sqrt[2].
-   * @tparam T
+   * @tparam TArithmetic
    */
   template <typename TArithmetic>
   inline constexpr TArithmetic One_div_sqrt_2 { 0.707106781186547524400844362104849039l };
